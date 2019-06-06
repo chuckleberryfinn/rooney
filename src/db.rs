e87a2601d4621fe453871518e09c18d413adf489
Cathal Mullaney <cmullaney@tenable.com>
2019-06-06 21:36:46 +0100

    Drop unnecessary format

--- src/db.rs
+++ src/db.rs
@@ -43,45 +43,45 @@ impl DB {
     }
 
     pub fn get_latest_price(&self, coin: String) -> Option<Price> {
-        let query = format!(
-                            "with daily_prices as (
-                                select * from prices
-                                join coins using(coin_id)
-                                where time >= (select max(time::date) from prices)
-                                and name = $1
-                                order by time asc
-                            ),
-                            min_max_prices as (
-                                select name, max(euro), min(euro)
-                                from daily_prices
-                                group by name
-                            ),
-                            all_prices as (
-                                select row_number() over (partition by name order by time desc)
-                                rn, name, ticker, euro, dollar, time
-                                from daily_prices
-                            ),
-                            median_prices as (
-                                select name, median(euro) as median
-                                from all_prices
-                                group by name
-                            ),
-                            latest_prices as (
-                                select name, ticker, euro, dollar
-                                from all_prices
-                                where rn = 1
-                            ),
-                            first_price as (
-                                select name, euro from daily_prices
-                                where name=$1
-                                limit 1
-                            )
-                            select name, ticker, cast(lp.euro as real), cast(dollar as real), cast(min as real), cast(max as real),
-                            cast(((lp.euro - fp.euro)*100)/fp.euro as real), cast(median as real)
-                            from latest_prices as lp
-                            join min_max_prices using(name)
-                            join first_price as fp using(name)
-                            join median_prices using(name)");
+        let query =
+                    "with daily_prices as (
+                        select * from prices
+                        join coins using(coin_id)
+                        where time >= (select max(time::date) from prices)
+                        and name = $1
+                        order by time asc
+                    ),
+                    min_max_prices as (
+                        select name, max(euro), min(euro)
+                        from daily_prices
+                        group by name
+                    ),
+                    all_prices as (
+                        select row_number() over (partition by name order by time desc)
+                        rn, name, ticker, euro, dollar, time
+                        from daily_prices
+                    ),
+                    median_prices as (
+                        select name, median(euro) as median
+                        from all_prices
+                        group by name
+                    ),
+                    latest_prices as (
+                        select name, ticker, euro, dollar
+                        from all_prices
+                        where rn = 1
+                    ),
+                    first_price as (
+                        select name, euro from daily_prices
+                        where name=$1
+                        limit 1
+                    )
+                    select name, ticker, cast(lp.euro as real), cast(dollar as real), cast(min as real), cast(max as real),
+                    cast(((lp.euro - fp.euro)*100)/fp.euro as real), cast(median as real)
+                    from latest_prices as lp
+                    join min_max_prices using(name)
+                    join first_price as fp using(name)
+                    join median_prices using(name)";
 
         for row in &self.connection.query(&query, &[&coin]).unwrap() {
             return Some(Price {
