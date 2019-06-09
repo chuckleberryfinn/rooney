23ae1f18abc0bc4b1508b43ba88180e6051d174a
Cathal Mullaney <cmullaney@tenable.com>
2019-06-09 04:18:27 +0100

    Make format methods private

--- src/replies.rs
+++ src/replies.rs
@@ -100,7 +100,7 @@ impl Replies {
         None
     }
 
-    pub fn format_currency(value: f32) -> String {
+    fn format_currency(value: f32) -> String {
         if value < 1.0 {
             return format!("{:.8}", value);
         }
@@ -110,7 +110,7 @@ impl Replies {
         v.separated_string()
     }
 
-    pub fn format_change(diff: f32) -> String {
+    fn format_change(diff: f32) -> String {
         if diff < 0.0 {
             return format!("\x0305Down: {:.2}%", diff.abs());
         }
