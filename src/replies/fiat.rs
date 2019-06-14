2a77a3aa1e63c23cd7ccc7cc042a271fea7008e0
Cathal Mullaney <cmullaney@tenable.com>
2019-06-14 19:31:29 +0100

    Use super inline

--- src/replies/fiat.rs
+++ src/replies/fiat.rs
@@ -1,8 +1,6 @@
 use titlecase::titlecase;
 
-use super::db;
-
-pub fn get_fiat(db: &db::DB, coin: String, amount: f32) -> Option<String> {
+pub fn get_fiat(db: &super::db::DB, coin: String, amount: f32) -> Option<String> {
     let price = db.get_latest_price(coin);
     if let Some(p) = price {
         return Some(format!("{} {} ({}) is worth €{} at €{} per coin", amount, titlecase(&p.name), p.ticker.to_uppercase(),
