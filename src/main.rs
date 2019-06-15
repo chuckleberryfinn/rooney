1ca334386b780977de551556046177478d7b75fe
Cathal Mullaney <cmullaney@tenable.com>
2019-06-15 23:01:41 +0100

    Split main into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,3 @@
-#[macro_use]
-extern crate log;
-extern crate std_logger;
-
-mod db;
-mod replies;
-
-use irc::client::prelude::*;
-
 fn main() {
-    let config = Config::load("configuration/config.toml").unwrap();
-
-    let mut reactor = IrcReactor::new().unwrap();
-    let client = reactor.prepare_client_and_connect(&config).unwrap();
-    let replies = replies::Replies::new();
-    std_logger::init();
-
-    client.identify().unwrap();
-
-    reactor.register_client_with_handler(client, move |client, message| {
-        if let Command::PRIVMSG(ref target, ref msg) = message.command {
-            match replies.handle_message(&msg) {
-                Some(response) => client.send_privmsg(message.response_target().unwrap(), response)?,
-                None => (),
-            }
-            info!("{} said {} to {}", message.source_nickname().unwrap(), msg, target);
-        }
-
-        Ok(())
-    });
-
-    reactor.run().unwrap();
+    rooney::run()
 }
