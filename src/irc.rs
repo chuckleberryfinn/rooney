5822d5c6538643c77e0a40567efca8249211f07d
nemo-rb <42155140+nemo-rb@users.noreply.github.com>
2020-04-26 18:46:41 +0100

    Refactor main processing loop
    Refactor main processing loop.
    
    Co-authored-by: Chuckles <chuckleberryfinn@gmail.com>

---
+++ src/irc.rs
@@ -0,0 +1,32 @@
+use crate::{commands, Error, Messenger, Result};
+
+use irc::client::prelude::*;
+use irc::error::IrcError;
+use log::{info, warn};
+
+impl Messenger for IrcClient {
+    fn init(&self) -> Result<()> {
+        Ok(self.identify()?)
+    }
+
+    fn run(&self, handler: impl Fn(&str) -> commands::Result<String>) -> Result<()> {
+        Ok(self.for_each_incoming(|message| {
+            if let Command::PRIVMSG(ref target, ref msg) = message.command {
+                match handler(&msg) {
+                    Ok(response) => {
+                        info!("{}: {}", target, response);
+                        self.send_privmsg(message.response_target().unwrap(), &response)
+                            .unwrap_or_else(|e| warn!("{}", e))
+                    }
+                    Err(e) => warn!("{}: {}", target, e),
+                }
+            }
+        })?)
+    }
+}
+
+impl From<IrcError> for Error {
+    fn from(e: IrcError) -> Error {
+        Error::Messenger(Box::new(e))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,25 @@
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-
-mod db;
 mod commands;
+mod db;
+mod irc;
 
-use std::env;
+use failure::Fail;
 
-use irc::client::prelude::*;
-
-pub fn run() {
-    let args: Vec<String> = env::args().collect();
-
-
-    let config = if args.len() == 1 {
-        Config::load("configuration/DebugConfig.toml").unwrap()
-    } else {
-        match args[1].as_str() {
-            "release" => Config::load("configuration/Config.toml").unwrap(),
-            _ => Config::load("configuration/DebugConfig.toml").unwrap()
-        }
-    };
-
-    let mut reactor = IrcReactor::new().unwrap();
-    let client = reactor.prepare_client_and_connect(&config).unwrap();
+pub fn bot(messenger: impl Messenger) -> Result<()> {
     let commands = commands::Commands::new();
 
-    client.identify().unwrap();
-
-    reactor.register_client_with_handler(client, move |client, message| {
-        if let Command::PRIVMSG(ref target, ref msg) = message.command {
-            match commands.handle(&msg) {
-                Ok(response) => client.send_privmsg(message.response_target().unwrap(), response)?,
-                Err(e) => info!("Error encountered: {:?}", e),
-            }
-            info!("{} said {} to {}", message.source_nickname().unwrap(), msg, target);
-        }
-
-        Ok(())
-    });
-
-    reactor.run().unwrap();
+    messenger.init()?;
+    messenger.run(|m| commands.handle(m))
 }
+
+pub trait Messenger {
+    fn init(&self) -> Result<()>;
+    fn run(&self, handler: impl Fn(&str) -> commands::Result<String>) -> Result<()>;
+}
+
+#[derive(Debug, Fail)]
+pub enum Error {
+    #[fail(display = "Messenger error {}", _0)]
+    Messenger(#[cause] Box<dyn Fail>),
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
