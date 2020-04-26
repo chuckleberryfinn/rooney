85137482a574b49ad0b72fb373e0e4b817279515
Chuckles <chuckleberryfinn@gmail.com>
2020-04-26 18:17:12 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
-fn main() {
+use std::env;
+
+fn main() -> rooney::Result<()> {
     env_logger::init();
-    rooney::run()
+    let args: Vec<String> = env::args().collect();
+
+    let config = if args.len() == 1 {
+        "configuration/DebugConfig.toml"
+    } else {
+        match args[1].as_str() {
+            "release" => "configuration/Config.toml",
+            _ => "configuration/DebugConfig.toml"
+        }
+    };
+    
+    rooney::bot(irc::client::prelude::IrcClient::new(&config)?)
 }
