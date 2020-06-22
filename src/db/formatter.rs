855101aa5ecc093eeb79297cfca6a6d8d863a911
Chuckles <chuckleberryfinn@gmail.com>
2020-06-22 17:27:09 +0100

    Fix tests

--- src/db/formatter.rs
+++ src/db/formatter.rs
@@ -36,26 +36,10 @@ mod tests {
         }
     }
 
-    macro_rules! format_change_tests {
-        ($($name:ident: $value:expr,)*) => {
-        $(
-            #[test]
-            fn $name() {
-                let (input, expected) = $value;
-                assert_eq!(expected, format_change(input));
-            }
-        )*
-        }
-    }
-
     format_currency_tests! {
         format_currency_0: (1.0, "1.00"),
         format_currency_1: (0.012345, "0.01234500"),
         format_currency_2: (1.0, "1.00"),
     }
 
-    format_change_tests! {
-        format_change_0: (1.0, "\u{3}03Up: 1.00%"),
-        format_change_1: (-50.0512, "\u{3}05Down: 50.05%"),
-    }
 }
