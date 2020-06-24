ad6eab2b9fa9b5c041af5b22d74a6cb4619be69a
Chuckles <chuckleberryfinn@gmail.com>
2020-06-24 23:33:14 +0100

    Add initial updater code

--- src/bin/updater.rs
+++ src/bin/updater.rs
@@ -1,6 +1,18 @@
 use log::info;
+use std::io::Read;
 
-fn main() {
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     info!("Updater");
+    let url = "https://www.worldcoinindex.com/apiservice/json";
+    let mut res = reqwest::blocking::get(url)?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    println!("Body:\n{}", body);
+
+    Ok(())
 }
