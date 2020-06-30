c3c1d8f1a3b6f9576eac14cc8266d9520b52c5fa
Chuckles <chuckleberryfinn@gmail.com>
2020-06-30 14:32:36 +0100

    Refactor error handling

--- src/bin/updater.rs
+++ src/bin/updater.rs
@@ -1,15 +1,10 @@
+use std::{collections::HashMap, error::Error, fmt, fs, io::Read, thread, time};
+
 use rooney::db;
 
 use log::{error, info};
-use std::collections::HashMap;
-use std::fs;
-use std::io::Read;
-use std::{thread, time};
-use toml::Value;
-
-extern crate rustc_serialize;
-
 use rustc_serialize::json;
+use toml::Value;
 
 
 #[allow(non_snake_case)]
@@ -35,6 +30,22 @@ impl Market {
 }
 
 
+#[derive(Debug)]
+struct JSONParseError {
+    message: String,
+}
+
+
+impl fmt::Display for JSONParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+
+impl Error for JSONParseError {}
+
+
 fn read_config(path: &str) -> Value {
     let toml_content = fs::read_to_string(path)
                         .unwrap_or_else(|_| panic!("Unable to read updater config from: {}", path));
@@ -42,58 +53,36 @@ fn read_config(path: &str) -> Value {
 }
 
 
-fn parse_json(url: &str) -> Result<Vec<Market>, String> {
-    let mut res = match reqwest::blocking::get(url) {
-        Ok(r) => r,
-        Err(e) => return Err(format!("Unable to read response from API: {}", e))
-    };
-
+fn get_json(url: &str) -> Result<String, Box<dyn Error>> {
+    let mut res = reqwest::blocking::get(url)?;
     let mut body = String::new();
-    match res.read_to_string(&mut body) {
-        Ok(_) => (),
-        Err(e) => return Err(format!("Unable to read response to string: {}", e))
-    };
 
-    let json: HashMap<String, Vec<Market>> = match json::decode(&body) {
-        Ok(h) => h,
-        Err(e) => return Err(format!("Unable to decode JSON: {}.", e))
-    };
+    res.read_to_string(&mut body)?;
+    Ok(body)
+}
 
+
+fn parse_json(body: &str) -> Result<Vec<Market>, Box<dyn Error>> {
+    let json: HashMap<String, Vec<Market>> = json::decode(&body)?;
     match json.get("Markets") {
         Some(m) => Ok(m.to_vec()),
-        None => Err("JSON has unexpected format.".to_string())
+        None => Err(Box::new(JSONParseError{ message: "Unable to parse markets".to_string() }))
     }
 }
 
 
-fn add_coins(db: &db::DB, markets: &[Market]) -> Result<(), String> {
+fn add_coins(db: &db::DB, markets: &[Market]) -> Result<(), Box<dyn Error>> {
     let args = markets.iter().map(|m| m.get_args()).collect::<Vec<_>>();
-    let transaction = match db.connection.transaction() {
-        Ok(t) => t,
-        Err(e) => return Err(format!("Failed to create transaction {}", e))
-    };
-
-    match transaction.batch_execute("Create temporary table temp_coins(name varchar(255), ticker varchar(100)) on commit drop") {
-        Ok(_) => (),
-        Err(e) => return Err(format!("Failed to create temp table {}", e))
-    };
+    let transaction = db.connection.transaction()?;
+    transaction.batch_execute("Create temporary table temp_coins(name varchar(255), ticker varchar(100)) on commit drop")?;
 
     for a in args {
-        match transaction.execute("Insert into temp_coins(name, ticker) values ($1, $2) ", &[&a.0, &a.1],) {
-            Ok(_) => (),
-            Err(e) => return Err(format!("Failed to insert into temp_coins table {}", e))
-        };
+        transaction.execute("Insert into temp_coins(name, ticker) values ($1, $2) ", &[&a.0, &a.1],)?;
     }
 
-    match transaction.batch_execute("Insert into coins(name, ticker) select tc.name, tc.ticker from temp_coins tc left join coins c using(name) where c.name is null") {
-        Ok(_) => (),
-        Err(e) => return Err(format!("Failed to insert into coins table {}", e))
-    };
-
-    match transaction.commit() {
-        Ok(_) => Ok(()),
-        Err(e) => Err(format!("Failed to complete transaction {}", e))
-    }
+    transaction.batch_execute("Insert into coins(name, ticker) select tc.name, tc.ticker from temp_coins tc left join coins c using(name) where c.name is null")?;
+    transaction.commit()?;
+    Ok(())
 }
 
 
@@ -105,66 +94,63 @@ fn coins_ids(db: &db::DB) -> Result<HashMap<String, i32>, String> {
 }
 
 
-fn update_prices(db: &db::DB, coins_ids: HashMap<String, i32>, markets: &[Market]) -> Result<(), String> {
+fn update_prices(db: &db::DB, coins_ids: HashMap<String, i32>, markets: &[Market]) -> Result<(), Box<dyn Error>> {
     //This whole function is here to work around the Postgres numeric type and it's incompatibility with Rust.
-    let transaction = match db.connection.transaction() {
-        Ok(t) => t,
-        Err(e) => return Err(format!("Failed to create transaction {}", e))
-    };
-
-    match transaction.batch_execute("Create temporary table temp_prices(coin_id integer, euro real, dollar real) on commit drop") {
-        Ok(_) => (),
-        Err(e) => return Err(format!("Failed to create temp table {}", e))
-    };
+    let transaction = db.connection.transaction()?;
+    transaction.batch_execute("Create temporary table temp_prices(coin_id integer, euro real, dollar real) on commit drop")?;
 
     for m in markets {
         let args = m.get_args();
         let coin_id = coins_ids.get(&args.0).unwrap();
-        match transaction.execute("Insert into temp_prices(coin_id, euro, dollar) values ($1, $2, $3) ", &[&coin_id, &args.3, &args.4],) {
-            Ok(_) => (),
-            Err(e) => return Err(format!("Failed to insert into temp_prices table {}", e))
-        };
+        transaction.execute("Insert into temp_prices(coin_id, euro, dollar) values ($1, $2, $3) ", &[&coin_id, &args.3, &args.4],)?;
     }
 
-    match transaction.batch_execute("Insert into prices(coin_id, euro, dollar) select coin_id, euro::numeric, dollar::numeric from temp_prices") {
-        Ok(_) => (),
-        Err(e) => return Err(format!("Failed to insert into prices table {}", e))
+    transaction.batch_execute("Insert into prices(coin_id, euro, dollar) select coin_id, euro::numeric, dollar::numeric from temp_prices")?;
+    transaction.commit()?;
+    Ok(())
+}
+
+
+fn get_updates(url: &str) -> Result<(), String> {
+    let json = match get_json(&url) {
+        Ok(j) => j,
+        Err(e) => return Err(format!("Unable to get JSON: {}", e))
     };
 
-    match transaction.commit() {
-        Ok(_) => Ok(()),
-        Err(e) => Err(format!("Failed to complete transaction {}", e))
+    let markets = match parse_json(&json) {
+        Ok(m) => m,
+        Err(e) => return Err(format!("Unable to parse JSON: {}", e))
+    };
+
+    let db = db::DB::new();
+    match add_coins(&db, &markets) {
+        Ok(()) => (),
+        Err(e) => return Err(format!("Unable to add coins: {}", e))
+    };
+
+    let coins_ids = coins_ids(&db)?;
+    match update_prices(&db, coins_ids, &markets) {
+        Ok(()) => Ok(()),
+        Err(e) => Err(format!("Unable to update prices: {}", e))
     }
 }
 
 
-fn run_updater(url: &str) -> Result<(), String> {
-    let markets = parse_json(&url)?;
-    let db = db::DB::new();
-    add_coins(&db, &markets)?;
-    let coins_ids = coins_ids(&db)?;
-
-    update_prices(&db, coins_ids, &markets)
-}
-
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     let five_mins = time::Duration::from_secs(5*60);
     let config = read_config("configuration/Updater.toml");
-    let key = config["updater"]["api_key"].as_str().unwrap();
+    let key = config["updater"]["api_key"].as_str().expect("Updater.toml must include an api_key field in the updater section.");
     let url = format!("https://www.worldcoinindex.com/apiservice/json?key={}", key);
 
     loop {
         info!("Get updated price");
 
-        match run_updater(&url) {
+        match get_updates(&url) {
             Ok(_) => (),
             Err(e) => error!("An unexpected error occurred: {}", e)
         };
 
         thread::sleep(five_mins);
     }
-
-    Ok(())
 }
